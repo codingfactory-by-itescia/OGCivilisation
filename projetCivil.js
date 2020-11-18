d18d9632ae26e8bfbe90f5ac9b6a27f14ea8a7af
Nathan Lecorbeiller <lecorbeiller.nathan@outlook.fr>
2020-11-18 09:55:35 +0100

    essai bouton JS

--- projetCivil.js
+++ projetCivil.js
@@ -1,5 +1,18 @@
-document.getElementById("city").addEventListener("click", inventaireClick);
+document.getElementById("city").addEventListener("click", cityClick);
 
 document.getElementById("map").addEventListener("click", mapClick);
 
 document.getElementById("inventory").addEventListener("click", inventoryClick);
+
+cityClick
+{
+    console.log("Ville");
+}
+mapClick
+{
+    console.log("Carte");
+}
+inventoryClick
+{
+    console.log("Inventaire");
+}
