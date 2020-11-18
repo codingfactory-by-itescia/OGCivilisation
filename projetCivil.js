343126b92cf5fe1b321290eaf2e522a1bb34e4ac
Alexandre <alexbx2002@gmail.com>
2020-11-18 14:05:03 +0100

    rajout des barres de progression

--- projetCivil.js
+++ projetCivil.js
@@ -1,1 +1,7 @@
 //window.location.href="https://www.youtube.com/watch?v=fC7oUOUEEi4"
+let ble = document.getElementById("ble")
+ble.value -= 10;
+let or = document.getElementById("or")
+or.value -= 40;
+let troupe = document.getElementById("troupe")
+troupe.value -= 30;
