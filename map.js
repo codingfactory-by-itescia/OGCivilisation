bfddd7069bd08bc269c68fc53f8206f38544b4ca
Alexandre <alexbx2002@gmail.com>
2020-11-18 09:53:16 +0100

    Auto stash before merge of "Branche-test" and "origin/Branche-test"
    rajout de la map

---
+++ map.js
@@ -0,0 +1,28 @@
+
+   type="text/javascript">
+      google.charts.load('current', {
+        'packages':['geochart'],
+        // Note: you will need to get a mapsApiKey for your project.
+        // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+        'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+      });
+      google.charts.setOnLoadCallback(drawRegionsMap);
+
+      function drawRegionsMap() {
+        var data = google.visualization.arrayToDataTable([
+          ['Country', 'Army'],
+          ['China', 200],
+          ['Japan', 300],
+          ['France', 400],
+          ['South Africa', 500],
+          ['Colombia', 600],
+          ['Denmark', 700]
+        ]);
+
+        var options = {};
+
+        var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+        chart.draw(data, options);
+      }
+      
