3f5ea6a2fc6282d499077c9886fbcb04ca3af53b
Mancone Alexis <ale.mancone@gmail.com>
2020-11-19 11:59:49 +0100

    bouton bâtiments fonctionnel

--- projetCivil.js
+++ projetCivil.js
@@ -5,23 +5,23 @@ or.value -= 40;
 let troupe = document.getElementById("troupe")
 troupe.value -= 30;
 
-function progressBarSim(al) {
-    var bar = document.getElementById('barble');
-    var status = document.getElementById('status');
+/*function progressBarSim(al) {
+    let bar = document.getElementById('barble');
+    let status = document.getElementById('status');
     status.innerHTML = al+"%";
     bar.value = al;
     al++;
-      var sim = setTimeout("progressBarSim("+al+")",300);
+      let sim = setTimeout("progressBarSim("+al+")",300);
       if(al == 100){
         status.innerHTML = "100%";
         bar.value = 100;
         clearTimeout(sim);
-        var finalMessage = document.getElementById('finalMessage');
+        let finalMessage = document.getElementById('finalMessage');
         finalMessage.innerHTML = "Process is complete";
       }
   }
-var amountLoaded = 0;
-progressBarSim(amountLoaded);
+let amountLoaded = 0;
+progressBarSim(amountLoaded);*/
 
 //bouton du sous menu de batiments
 let fond=document.getElementById("fond")
@@ -52,11 +52,10 @@ let army={
 function farmMenu(){
     fond.src = farm.fond
     menu.innerHTML = farm.menu
-    console.log(event.target.id)
 }
 function caveMenu(){
-    fond = cave.fond
-    menu = cave.menu
+    fond.src = cave.fond
+    menu.innerHTML = cave.menu
 }
 function armyMenu(){
     fond.src = army.fond
