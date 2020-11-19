ace8fa4c1294d5deea422ad939334ce9ba91660a
TortueBleu <klarir.games@gmail.com>
2020-11-19 11:57:34 +0100

    sauvegarde midi

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,10 @@ const app = express();
 const port = 4000;
 
 app.listen(port, () => {
-  console.log(Success! Your application is running on port ${port}.);
-})
-var express = require('express');
-var app = express();
+  console.log(`Success! Your application is running on port ${port}.`);
+});
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world');
+app.get('/test', function(req, res) {
+  res.send('ça fonctionne');
 });
