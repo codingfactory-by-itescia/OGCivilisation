2e7bd7052fb65aa319b4ac9379363259adaf3acd
TortueBleu <klarir.games@gmail.com>
2020-11-21 17:40:24 +0100

    odif perso antoine

--- unites.js
+++ unites.js
@@ -1,89 +1,79 @@
-let button1 = document.getElementById("archers");
-let archer = document.getElementById("archer1");
-let i = 5;
+const button1 = document.getElementById("archers");
+const archer = document.getElementById("archer1");
+let i = 0;
 
-button1.addEventListener('click',troupes1)
+const button2 = document.getElementById("assassins");
+const assassin = document.getElementById("assassin1");
+let j = 0;
 
-function troupes1(event) {
-    
-    archer.innerHTML = i;
-    i = i + 5;
-    button1.disabled = true;
-    setTimeout(() => {
+const button3 = document.getElementById("valkyries");
+const valkyrie = document.getElementById("valkyrie1");
+let r = 0;
+
+const button4 = document.getElementById("pyros");
+const pyro = document.getElementById("pyro1");
+let s = 0;
+
+const button5 = document.getElementById("vikings");
+const viking = document.getElementById("viking1");
+let q = 0;
+
+
+const creationTroupe = (event) => {
+  switch (event.target.id) {
+    case "archers":
+
+      i = i + 5;
+      button1.disabled = true;
+      setTimeout(() => {
         button1.disabled = false;
-
+        archer.innerHTML = i;
       }, 10000)
+      break;
 
-}
-
-let button2 = document.getElementById("assassins");
-let assassin = document.getElementById("assassin1");
-let j = 3;
-
-button2.addEventListener('click',troupes2)
-
-function troupes2(event) {
-    
-    assassin.innerHTML = j;
-    j = j + 3;
-    button2.disabled = true;
-    setTimeout(() => {
+    case "assassins":
+      j = j + 3;
+      button2.disabled = true;
+      setTimeout(() => {
         button2.disabled = false;
-
+        assassin.innerHTML = j;
       }, 30000)
 
-}
+      break;
 
-let button3 = document.getElementById("valkyries");
-let valkyrie = document.getElementById("valkyrie1");
-let r = 2;
-
-button3.addEventListener('click',troupes3)
-
-function troupes3(event) {
-    
-    valkyrie.innerHTML = r;
-    r = r + 2;
-    button3.disabled = true;
-    setTimeout(() => {
+    case "valkyries":
+      r = r + 2;
+      button3.disabled = true;
+      setTimeout(() => {
         button3.disabled = false;
-
+        valkyrie.innerHTML = r;
       }, 60000)
 
-}
+      break;
 
-let button4 = document.getElementById("pyros");
-let pyro = document.getElementById("pyro1");
-let s = 1;
-
-button4.addEventListener('click',troupes4)
-
-function troupes4(event) {
-    
-    pyro.innerHTML = s;
-    s = s + 1;
-    button4.disabled = true;
-    setTimeout(() => {
+    case "pyros":
+      s = s + 1;
+      button4.disabled = true;
+      setTimeout(() => {
         button4.disabled = false;
-
+        pyro.innerHTML = s;
       }, 600000)
+      break;
 
-}
-
-let button5 = document.getElementById("vikings");
-let viking = document.getElementById("viking1");
-let q = 1;
-
-button5.addEventListener('click',troupes5)
-
-function troupes5(event) {
-    
-    viking.innerHTML = q;
-    q = q + 1;
-    button5.disabled = true;
-    setTimeout(() => {
+    case "vikings":
+      q = q + 1;
+      button5.disabled = true;
+      setTimeout(() => {
         button5.disabled = false;
-
+        viking.innerHTML = q;
       }, 1800000)
-
+      break
+  }
 }
+
+
+button1.addEventListener('click', creationTroupe);
+button2.addEventListener('click', creationTroupe);
+button3.addEventListener('click', creationTroupe);
+button4.addEventListener('click', creationTroupe);
+button5.addEventListener('click', creationTroupe);
